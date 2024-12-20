b41b6446e63e40ed31224f7f13e348c1ffb6d7a9
omegafrog <jiwoo22110103@gmail.com>
2024-12-20 10:00:21 +0900

    refact : app routing 리팩토링
    화드코딩된 명령어를 enum 사용해 ide도움 받을 수 있도록 수정함.
    
    parameter parsing을 app에서 전체적으로 수행해 controller에는 실제로 사용할 타입의 값만 넘어갈 수 있도록 수정
    
    parameter parsing중에 하드코딩된 분리자 (?, &)와 파라미터 key값을 enum으로 변경

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,18 +1,68 @@
 package org.example;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
-
-import static java.lang.System.exit;
 
 public class App {
     private final WiseSayingController controller;
     private final Scanner scanner;
+
+    private enum COMMAND{
+        EXIT("종료"),
+        CREATE("등록"),
+        LIST("목록"),
+        DELETE("삭제"),
+        UPDATE("수정"),
+        BUILD("빌드")
+        ;
+        String name;
+        COMMAND(String name) {
+            this.name = name;
+        }
+
+        static COMMAND of(String name){
+            switch (name){
+                case "등록" -> {
+                    return COMMAND.CREATE;
+                }
+                case "종료" -> {
+                    return COMMAND.EXIT;
+                }
+                case "목록" -> {
+                    return COMMAND.LIST;
+                }
+                case "수정" -> {
+                    return COMMAND.UPDATE;
+                }
+                case "빌드" -> {
+                    return COMMAND.BUILD;
+                }
+                case "삭제" -> {
+                    return COMMAND.DELETE;
+                }
+                default -> throw new IllegalArgumentException("Unknown command: " + name);
+            }
+        }
+    }
+    
+    public enum PARAMETERS{
+        KEYWORD_TYPE("keywordType"),KEYWORD("keyword"),PAGE_NUM("page"),
+        ID("id"), OPERATOR_SPLITTER("\\?"),PARAMETER_SPLITTER("&"), PARAMETER_VALUE_SPLITTER("="),;
+        String value;
+        PARAMETERS(String value) {
+            this.value = value;
+        }
+    }
 
     public App(WiseSayingController controller, Scanner scanner) {
         this.controller = controller;
         this.scanner = scanner;
     }
 
+    /**
+     * 사용자로부터 명령 입력을 받아 세부 로직으로 라우팅함.
+     */
     public void run(){
         System.out.println("== 명언 앱 == ");
         while(true){
@@ -25,28 +75,51 @@ public class App {
             }
             String input = scanner.nextLine();
 
+            Map<String, String> parsedParameterMap = getParsedParameterMap(input);
+            COMMAND operator = COMMAND.of(parsedParameterMap.get("operator"));
 
-            String[] split = input.split("\\?");
-            String operator = split[0];
-            String parameter = "";
-            if(split.length == 2)
-                parameter = split[1];
-
-            switch (operator) {
-                case "종료" -> controller.exit();
-                case "등록" -> controller.create();
-                case "목록" -> {
-                    if( parameter.length() > 0){
-                        controller.findBy(parameter);
-                        break;
-                    }
-                    controller.readAll(1);
-                }
-                case "삭제" -> controller.delete(parameter);
-                case "수정" -> controller.update(parameter);
-                case "빌드" -> controller.build();
-                default -> throw new IllegalArgumentException("Unknown operator: " + input);
-            }
+            route(operator, parsedParameterMap, input);
         }
+    }
+
+    private void route(COMMAND operator, Map<String, String> parsedParameterMap, String input) {
+        switch (operator) {
+            case EXIT -> controller.exit();
+            case CREATE -> controller.create();
+            case LIST -> {
+                if(parsedParameterMap.containsKey("keywordType") && parsedParameterMap.containsKey("keyword"))
+                    controller.search(parsedParameterMap.get("keywordType"),
+                            parsedParameterMap.get("keyword"),
+                            Integer.parseInt(parsedParameterMap.get("page")));
+                else
+                    controller.readAll(Integer.parseInt(parsedParameterMap.get("page")));
+            }
+            case DELETE -> controller.delete(Integer.parseInt(parsedParameterMap.get(PARAMETERS.ID.value)));
+            case UPDATE -> controller.update(Integer.parseInt(parsedParameterMap.get(PARAMETERS.ID.value)));
+            case BUILD -> controller.build();
+        }
+    }
+
+    private static Map<String, String> getParsedParameterMap(String input){
+        Map<String, String> parameterMap = new HashMap<>();
+        parameterMap.put(PARAMETERS.PAGE_NUM.value, "1");
+
+
+        String[] split = input.split(PARAMETERS.OPERATOR_SPLITTER.value);
+        String operator = split[0];
+        parameterMap.put("operator",operator);
+
+        String parameter = split.length == 2?split[1]:"";
+
+        if(parameter.isEmpty())
+            return parameterMap;
+        String[] parsedParams = parameter.split(PARAMETERS.PARAMETER_SPLITTER.value);
+
+        for(String param : parsedParams)
+            parameterMap.put(param.split(
+                    PARAMETERS.PARAMETER_VALUE_SPLITTER.value)[0],
+                    param.split(PARAMETERS.PARAMETER_VALUE_SPLITTER.value)[1]);
+
+        return parameterMap;
     }
 }
